cdf2a305856f7d3831429e1a7d10966e93f1cb3f
inouyeg23 <74692328+inouyeg23@users.noreply.github.com>
2022-03-26 19:39:03 -0700

    Update tracer.py
    added secure check

--- tracer.py
+++ tracer.py
@@ -9,3 +9,10 @@ capture.sniff(timeout = time)
 
 for packet in capture.sniff_continuously(packet_count=capture.size):
     print(packet.dns)
+
+
+def checkSecure(url):
+    if(url.find('https') != -1):
+        print("secure webapge")
+    else:
+        print("non-secure")
