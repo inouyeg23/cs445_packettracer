97f2191fa38f90a75663abd66159fc7b6ad17920
njpon <70611659+njpon@users.noreply.github.com>
2022-03-27 21:47:46 -0700

    Pyshark basic tracing completed

--- tracer.py
+++ tracer.py
@@ -2,10 +2,11 @@ import socket
 import pyshark
 import dns
 
-time = input("Please enter a timeout value")
+print("Beginning capture: \n")
 
-capture = pyshark.LiveRingCapture()
-capture.sniff(timeout = time)
+capture = pyshark.LiveCapture(interface="en0",only_summaries=True)
+#capture.set_debug()
+capture.sniff(timeout=10)
 
-for packet in capture.sniff_continuously(packet_count=capture.size):
-    print(packet.dns)
+for packet in capture.sniff_continuously(packet_count=5):
+    print(packet)
