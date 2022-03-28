3458e4fa1d3072e9788c368b75e565f49629f257
hayesj23 <70611058+hayesj23@users.noreply.github.com>
2022-03-27 22:25:19 -0700

    update to only include dns packets

--- tracer.py
+++ tracer.py
@@ -4,9 +4,9 @@ import dns
 
 print("Beginning capture: \n")
 
-capture = pyshark.LiveCapture(interface="en0",only_summaries=True)
+capture = pyshark.LiveCapture(interface="en0",only_summaries=False, display_filter='dns')
 #capture.set_debug()
-capture.sniff(timeout=10)
+capture.sniff(timeout=20)
 
 for packet in capture.sniff_continuously(packet_count=5):
     print(packet)
